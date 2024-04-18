c0b02c8f759a75c0c45675f29006486d477d7310
Gammasoft <gammasoft71@gmail.com>
2024-04-18 08:28:55 +0200

    Update ApplicationUsingNativeTitleBar.cpp

--- juce_gui_basics/Applications/ApplicationUsingNativeTitleBar/src/ApplicationUsingNativeTitleBar.cpp
+++ juce_gui_basics/Applications/ApplicationUsingNativeTitleBar/src/ApplicationUsingNativeTitleBar.cpp
@@ -5,7 +5,7 @@ using namespace juce;
 namespace ApplicationExample {
   class Window1 : public DocumentWindow {
   public:
-    Window1() : DocumentWindow("Application using native title bar", Desktop::getInstance().getDefaultLookAndFeel().findColour(ResizableWindow::backgroundColourId), DocumentWindow::allButtons) {
+    Window1() : DocumentWindow {"Application using native title bar", Desktop::getInstance().getDefaultLookAndFeel().findColour(ResizableWindow::backgroundColourId), DocumentWindow::allButtons} {
       setResizable(true, true);
       setBounds(200, 100, 300, 300);
       setUsingNativeTitleBar(true);
