2024bcfb819d958626cc9476fdee48591f546b9b
Gammasoft <gammasoft71@gmail.com>
2024-04-18 16:41:04 +0200

    Add HelloWorldSay example

---
+++ juce_gui_basics/HelloWorlds/HelloWorldSay/src/HelloWorldSay.cpp
@@ -0,0 +1,60 @@
+#include <filesystem>
+#include <fstream>
+#include <juce_gui_basics/juce_gui_basics.h>
+
+using namespace juce;
+using namespace std;
+using namespace std::filesystem;
+
+namespace HelloWorldLabelExample {
+  class Window1 : public DocumentWindow {
+  public:
+    Window1() : DocumentWindow {"Hello world (Say)", Desktop::getInstance().getDefaultLookAndFeel().findColour(ResizableWindow::backgroundColourId), DocumentWindow::allButtons} {
+      setContentOwned(&mainComponent, false);
+      setResizable(true, true);
+      setBounds(200, 100, 300, 300);
+
+      button1.setBounds(10, 10, 75, 25);
+      button1.setButtonText("Say...");
+      button1.onClick = [&] {
+        Speak("Hello, World!");
+      };
+      
+      mainComponent.addAndMakeVisible(&button1);
+   }
+    
+  protected:
+    void closeButtonPressed() override {JUCEApplication::getInstance()->quit();}
+    
+  private:
+    // Very basic speech synthesizer using the command line.
+    void Speak(const String& text) {
+      static auto initialized = false;
+      if (!initialized) {
+        auto file = ofstream {temp_directory_path() / "say.cmd"};
+        if ((SystemStats::getOperatingSystemType() & SystemStats::Windows) == SystemStats::Windows) file << "@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak %* >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs";
+        else if ((SystemStats::getOperatingSystemType() & SystemStats::MacOSX) == SystemStats::MacOSX) file << "say $*";
+        else file << "spd-say $*";
+        permissions(temp_directory_path() / "say.cmd", perms::owner_all);
+        initialized = true;
+      }
+      Process::openDocument((temp_directory_path() / "say.cmd").string(), text);
+    }
+    Component mainComponent;
+    TextButton button1;
+  };
+  
+  class Application : public JUCEApplication {
+  protected:
+    const String getApplicationName() override {return JUCE_APPLICATION_NAME_STRING;}
+    const String getApplicationVersion() override {return JUCE_APPLICATION_VERSION_STRING;}
+    
+    void initialise(const String& commandLine) override {mainWindow.setVisible(true);}
+    void shutdown() override {}
+    
+  private:
+    Window1 mainWindow;
+  };
+}
+
+START_JUCE_APPLICATION(HelloWorldLabelExample::Application)
--- juce_gui_basics/HelloWorlds/README.md
+++ juce_gui_basics/HelloWorlds/README.md
@@ -4,10 +4,11 @@
 
 * [HelloWorldAlertWindow](HelloWorldAlertWindow/README.md) The classic first application HelloWorld with [juce::AlertWindow](https://docs.juce.com/master/classAlertWindow.html) dialog.
 * [HelloWorldAlertWindow2](HelloWorldAlertWindow2/README.md) The classic first application HelloWorld with [juce::AlertWindow](https://docs.juce.com/master/classAlertWindow.html) dialog.
-* [HelloWorldLabel](HelloWorldLabel/README.md) The classic first application HelloWorld with [juce::Label](https://docs.juce.com/master/classLabel.html) class.
+* [HelloWorldLabel](HelloWorldLabel/README.md) The classic first application HelloWorld with [juce::Label](https://docs.juce.com/master/classLabel.html) widget.
 * [HelloWorldNativeMessageBox](HelloWorldNativeMessageBox/README.md) The classic first application HelloWorld with [juce::NativeMessageBox](https://docs.juce.com/master/classNativeMessageBox.html) dialog.
 * [HelloWorldNativeMessageBox2](HelloWorldNativeMessageBox2/README.md) The classic first application HelloWorld with [juce::NativeMessageBox](https://docs.juce.com/master/classNativeMessageBox.html) dialog.
 * [HelloWorldPaint](HelloWorldPaint/README.md) The classic first application HelloWorld with [juce::Component::paint](https://docs.juce.com/master/classComponent.html#a7cf1862f4af5909ea72827898114a182) method.
+* [HelloWorldSay](HelloWorldSay/README.md) The classic first application HelloWorld with [juce::TextButton](https://docs.juce.com/master/classTextButton.html) widget.
 
 ## Build and run
 
