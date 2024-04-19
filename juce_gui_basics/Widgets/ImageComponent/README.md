8e8c4591d944a87799884ea94ddc3123dfc0e5f9
Gammasoft <gammasoft71@gmail.com>
2024-04-19 22:13:20 +0200

    Add ImageComponent example

---
+++ juce_gui_basics/Widgets/ImageComponent/README.md
@@ -0,0 +1,56 @@
+# ImageComponent
+
+Shows how to create a Image widget with [juce::ImageComponent](https://docs.juce.com/master/classImageComponent.html).
+
+## Source
+
+[src/ImageComponent.cpp](src/ImageComponent.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+## Output
+
+![output](../../../docs/Pictures/ImageComponent.png)
+
+## Build and run
+
+To build this project, open "Terminal" and type following lines:
+
+### Windows :
+
+``` shell
+mkdir build && cd build
+cmake .. 
+start ImageComponent.sln
+```
+
+Select `ImageComponent` project and type Ctrl+F5 to build and run it.
+
+### macOS :
+
+``` shell
+mkdir build && cd build
+cmake .. -G "Xcode"
+open ./ImageComponent.xcodeproj
+```
+
+Select `ImageComponent` project and type Cmd+R to build and run it.
+
+### Linux with Code::Blocks :
+
+``` shell
+mkdir build && cd build
+cmake .. -G "CodeBlocks - Unix Makefiles"
+xdg-open ./ImageComponent.cbp > /dev/null 2>&1
+```
+
+Select `ImageComponent` project and type F9 to build and run it.
+
+### Linux :
+
+``` shell
+mkdir build && cd build
+cmake .. 
+cmake --build . --config Debug
+./ImageComponent
+```
---
+++ juce_gui_basics/Widgets/ImageComponent/src/ImageComponent.cpp
@@ -0,0 +1,71 @@
+#include <juce_gui_basics/juce_gui_basics.h>
+
+using namespace juce;
+
+namespace ComboBoxExample {
+  class Window1 : public DocumentWindow {
+  public:
+    Window1() : DocumentWindow {"ImageComponent example", Desktop::getInstance().getDefaultLookAndFeel().findColour(ResizableWindow::backgroundColourId), DocumentWindow::allButtons} {
+      setBounds(200, 100, 300, 365);
+      setContentOwned(&mainComponent, false);
+      setResizable(true, true);
+      
+      comboBox1.setBounds(20, 20, 260, 25);
+      comboBox1.addItemList({"top-left", "top-right", "bottom-left", "bottom-right", "Center" , "Stretch to fit", "Fill destination", "Only reduce size", "Only Increase size", "Do not resize"}, 1);
+      comboBox1.onChange = [&] {
+        switch(comboBox1.getSelectedId()) {
+          case 1 : imageComponent1.setImagePlacement(RectanglePlacement::yTop | RectanglePlacement::xLeft | RectanglePlacement::doNotResize); break;
+          case 2 : imageComponent1.setImagePlacement(RectanglePlacement::yTop | RectanglePlacement::xRight | RectanglePlacement::doNotResize); break;
+          case 3 : imageComponent1.setImagePlacement(RectanglePlacement::yBottom | RectanglePlacement::xLeft | RectanglePlacement::doNotResize); break;
+          case 4 : imageComponent1.setImagePlacement(RectanglePlacement::yBottom | RectanglePlacement::xRight | RectanglePlacement::doNotResize); break;
+          case 5 : imageComponent1.setImagePlacement(RectanglePlacement::centred | RectanglePlacement::doNotResize); break;
+          case 6 : imageComponent1.setImagePlacement(RectanglePlacement::stretchToFit); break;
+          case 7 : imageComponent1.setImagePlacement(RectanglePlacement::fillDestination); break;
+          case 8 : imageComponent1.setImagePlacement(RectanglePlacement::onlyReduceInSize); break;
+          case 9 : imageComponent1.setImagePlacement(RectanglePlacement::onlyIncreaseInSize); break;
+          case 10 : imageComponent1.setImagePlacement(RectanglePlacement::doNotResize); break;
+        }
+      };
+      comboBox1.setSelectedId(1);
+      mainComponent.addAndMakeVisible(comboBox1);
+      
+      auto imagePath = File::getSpecialLocation(File::SpecialLocationType::currentExecutableFile).getParentDirectory();
+      if ((SystemStats::getOperatingSystemType() & SystemStats::MacOSX) == SystemStats::MacOSX) {
+        imagePath = imagePath.getParentDirectory();
+        imagePath = imagePath.getParentDirectory();
+        imagePath = imagePath.getParentDirectory();
+      }
+      imagePath = imagePath.getFullPathName() + File::getSeparatorString() + "Resources" + File::getSeparatorString() + "Logo.png";
+      imageComponent1.setBounds(20, 65, 260, 260);
+      imageComponent1.setImage(ImageFileFormat::loadFrom(imagePath), RectanglePlacement::yTop | RectanglePlacement::xLeft | RectanglePlacement::doNotResize);
+      mainComponent.addAndMakeVisible(imageComponent1);
+    }
+    
+  protected:
+    void closeButtonPressed() override {JUCEApplication::quit();}
+    void resized() override {
+      DocumentWindow::resized();
+      comboBox1.setSize(getWidth() - 40, comboBox1.getHeight());
+      imageComponent1.setSize(getWidth() - 40, getHeight() - 95);
+    }
+
+  private:
+    Component mainComponent;
+    ComboBox comboBox1;
+    ImageComponent imageComponent1;
+  };
+  
+  class Application : public JUCEApplication {
+  protected:
+    const String getApplicationName() override {return "";}
+    const String getApplicationVersion() override {return "";}
+    
+    void initialise(const String& commandLine) override {mainWindow.setVisible(true);}
+    void shutdown() override {}
+    
+  private:
+    Window1 mainWindow;
+  };
+}
+
+START_JUCE_APPLICATION(ComboBoxExample::Application)
